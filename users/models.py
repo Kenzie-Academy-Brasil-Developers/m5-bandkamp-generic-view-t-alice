6500c2a1accce7a65ef099a5fe39e4a382d1eeb4
Thaisa Alice <thaisalic3@icloud.com>
2023-02-24 15:37:02 -0300

    reafctor: serializers, view and user permission

--- users/models.py
+++ users/models.py
@@ -5,3 +5,13 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    email = models.EmailField(
+        max_length=127,
+        unique=True,
+        error_messages={"unique": "This field must be unique."},
+    )
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        error_messages={"unique": "A user with that username already exists."},
+    )
